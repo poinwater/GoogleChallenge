f4f3fe6639f2e067d31ddbfea4d4b08b2b4f011d
poinwater <10836861@qq.com>
2021-03-20 15:33:14 -0700

    Add Todos

--- app/src/main/java/com/example/googlechallenge/MainActivity.kt
+++ app/src/main/java/com/example/googlechallenge/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Todo: -Spencer Lock Screen Code
+        //Todo: -Kim Setting Time
+        //Todo: -Shanshan Gift Class
     }
 }
