acc50a7a0fc55537a1b8e2cc0c5a80c05002a735
poinwater <10836861@qq.com>
2021-03-21 15:06:36 -0700

    Add code for Gift interface

--- app/src/main/java/com/example/googlechallenge/GiftActivity.java
+++ app/src/main/java/com/example/googlechallenge/GiftActivity.java
@@ -3,6 +3,7 @@ package com.example.googlechallenge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class GiftActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class GiftActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gift);
+    }
+
+    public boolean isHealthySleeping = false;
+
+
+    public void openGift(View view){
+        //TODO: Shanshan Yu openGift
+    }
+
+    public Item[] getGift(boolean isHealthySleeping){
+        //TODO: Shanshan Yu the function randomly gets Gifts according to the condition
+        if (isHealthySleeping){
+            // do something
+        }else{
+            // do something
+        }
     }
 }
---
+++ app/src/main/java/com/example/googlechallenge/Item.java
@@ -0,0 +1,28 @@
+package com.example.googlechallenge;
+
+//TODO: ShanshanYu Finish the Item Class
+// Three items:
+// "Bronze Thread, thread1, 1, 5, 3",
+// "Silver Thread, thread2, 2, 10, 2"
+// "Gold Thread, thread3, 3, 15, 1"
+// -- We may need a database(?)
+
+public class Item {
+    String _name;
+    String _icon;
+    int _id;
+    int _value;
+    int _rare_level;
+
+    public Item(String name, String icon, int id, int value, int rare_level) {
+        this._name = name;
+        this._icon = icon;
+        this._id = id;
+        this._value = value;
+        this._rare_level = rare_level;
+    }
+
+    public int getValue {
+        return this._value;
+    }
+}
