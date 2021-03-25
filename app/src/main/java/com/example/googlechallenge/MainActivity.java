de5c3ecd573948c72006e49eaa34af236c7db7a6
poinwater <10836861@qq.com>
2021-03-25 10:16:21 -0700

    Change Buttons to Round Cornor Shape

--- app/src/main/java/com/example/googlechallenge/MainActivity.java
+++ app/src/main/java/com/example/googlechallenge/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.googlechallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+
+import static com.example.googlechallenge.R.drawable.round_corner;
 
 public class MainActivity extends AppCompatActivity {
 
