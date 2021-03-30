f3447b4fe76e437444f06c1f0c715033fed37c63
poinwater <10836861@qq.com>
2021-03-30 10:35:32 -0700

    Merge database

--- app/src/main/java/com/example/googlechallenge/DataActivity.java
+++ app/src/main/java/com/example/googlechallenge/DataActivity.java
@@ -2,17 +2,27 @@ package com.example.googlechallenge;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import com.example.googlechallenge.database.WordViewModel;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DataActivity extends AppCompatActivity {
+
+    public WordViewModel mWordViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +32,62 @@ public class DataActivity extends AppCompatActivity {
         BarChart barChart = (BarChart) findViewById(R.id.barchart);
 
         ArrayList<BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(5f, 0));
-        entries.add(new BarEntry(6f, 1));
-        entries.add(new BarEntry(7f, 2));
-        entries.add(new BarEntry(4f, 3));
-        entries.add(new BarEntry(5f, 4));
-        entries.add(new BarEntry(8f, 5));
-        entries.add(new BarEntry(7f, 6));
 
         BarDataSet bardataset = new BarDataSet(entries, "Cells");
 
         ArrayList<String> labels = new ArrayList<String>();
-        labels.add("Mon");
-        labels.add("Tue");
-        labels.add("Wed");
-        labels.add("Thur");
-        labels.add("Fri");
-        labels.add("Sat");
-        labels.add("Sun");
 
-        bardataset.setColors(new int[] {Color.YELLOW, Color.YELLOW, Color.GREEN, Color.RED, Color.RED, Color.GREEN, Color.GREEN});
+        ArrayList<Integer> color = new ArrayList<>();
 
-        BarData data = new BarData(labels, bardataset);
-        barChart.setData(data); // set the data and list of labels into chart
-        barChart.setDescription("Set Bar Chart Description Here");  // set the description
-        barChart.animateY(3000);
+
+        //access DB interface
+        mWordViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(WordViewModel.class);
+
+        // Add an observer on the LiveData returned by getAlphabetizedWords.
+        // The onChanged() method fires when the observed data changes and the activity is
+        // in the foreground.
+        mWordViewModel.getSleepDuration().observe(this, sleep -> {
+//        // Update the cached copy of the words in the adapter.
+//          //words is List typ
+            for(int i = 0 ; i < sleep.size() ; i++){
+                Log.d("test", "connectDatabase: "+sleep.get(i).getDuration());
+                entries.add(new BarEntry((sleep.get(i).getDuration()/60), i));
+                long hour = (sleep.get(i).getDuration()/60);
+                if( hour <= 4){
+                    color.add(Color.RED);
+                }else if(hour > 4 && hour <= 6 ){
+                    color.add(Color.YELLOW);
+                }else{
+                    color.add(Color.GREEN);
+                }
+
+                final Date[] time= new Date[1];
+                try{
+                    time[0] = DateFormat.getInstance().parse(sleep.get(i).getDate());
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.setTime(time[0]);
+                    SimpleDateFormat sdf = new SimpleDateFormat("EEE");
+                    String stringDate = sdf.format(time[0]);
+
+                    labels.add((time[0].getMonth()+1)+"/"+time[0].getDate()+", "+(time[0].getYear()+1900)+" "+stringDate);
+
+                }catch (Exception e){
+
+                }
+
+            }
+
+            bardataset.setColors(color);
+
+            BarData data = new BarData(labels, bardataset);
+
+            barChart.setData(data); // set the data and list of labels into chart
+            barChart.setDescription("Your Sleep Data Over the last 7 days");  // set the description
+            barChart.animateY(3000);
+    });
+//
+//
+
 
     }
 }
---
+++ app/src/main/java/com/example/googlechallenge/database/Sleep.java
@@ -0,0 +1,34 @@
+package com.example.googlechallenge.database;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "sleep_time_table")
+public class Sleep {
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "date")
+    private String date;
+
+    @NonNull
+    @ColumnInfo(name = "duration")
+    private long duration;
+
+    public Sleep(@NonNull String date, @NonNull long duration) {
+        this.date = date;
+        this.duration = duration;
+    }
+
+    @NonNull
+    public long getDuration() {
+        return this.duration;
+    }
+
+    @NonNull
+    public String getDate() {
+        return this.date;
+    }
+}
